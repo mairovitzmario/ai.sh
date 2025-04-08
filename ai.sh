0390905ceb51995bfd5a749a381d8d96db1263ec
mairovitzmario <mario10rafael@gmail.com>
2025-04-08 19:32:31 +0300

    added -n that shows message count

--- ai.sh
+++ ai.sh
@@ -19,8 +19,28 @@ check_mutual_exclusiveness() {
   FLAG_PRESENT=true
 }
 
+format_to_json() {
+  local txt=$1
+
+  txt=${txt%,}
+  txt="[$txt]"
+
+  echo "$txt"
+}
+
+
+format_from_json() {
+  local txt=$1
+
+  txt=${txt%]}   
+  txt=${txt#[}
+  txt="$txt,"
+
+  echo "$txt"
+}
+
 # Parse flags
-while getopts ":cslh" opt; do
+while getopts ":cslhn" opt; do
   case $opt in
     s)
       check_mutual_exclusiveness
@@ -30,6 +50,14 @@ while getopts ":cslh" opt; do
       check_mutual_exclusiveness
       mode="You are an AI model that needs to give long and detailed answers."
       ;;
+    n)
+      check_mutual_exclusiveness
+      CONTEXT=$(format_to_json "$CONTEXT")
+        
+      array_length=$(echo "$CONTEXT" | jq 'length')
+      echo "Your conversation has $array_length messages."
+      exit 0
+    ;;
     c)
       check_mutual_exclusiveness
       if [[ -z "$2" ]]; then # If -c is used with no arguments
@@ -44,22 +72,20 @@ while getopts ":cslh" opt; do
         #clear_count=$((clear_count * 2))
 
         # Turn the log file into a JSON array
-        CONTEXT=${CONTEXT%,}
-        CONTEXT="[$CONTEXT]"
+        CONTEXT=$(format_to_json "$CONTEXT")
         
-        # Remove first clear_count elements from the array
         array_length=$(echo "$CONTEXT" | jq 'length')
         
+        # Remove all elements if clear_count value is too big
         if [[ $clear_count -ge $array_length ]]; then
           clear_all
 
+        # Remove first clear_count elements from the array
         else 
           CONTEXT=$(echo "$CONTEXT" | jq ".[$clear_count:]")
 
           # Format it back
-          CONTEXT=${CONTEXT%]}   
-          CONTEXT=${CONTEXT#[}
-          CONTEXT="$CONTEXT,"
+          CONTEXT=$(format_from_json "$CONTEXT")
 
           echo "$CONTEXT" > "$LOG_PATH"
 
@@ -72,24 +98,25 @@ while getopts ":cslh" opt; do
     ;;
     h)
       check_mutual_exclusiveness
-      echo "Usage: $0 [-s | -l | -c <number>] [-h]"
+      echo "Usage: $0 [-s | -l <PROMPT> ] [-h | -c <number> | -n]"
       echo
       echo "Flags:"
       echo "  -s    Short answer (default)"
-      echo "  -l    Long and detailed answer"
+      echo "  -l    Long and detailed answer. You must provide a prompt after this flag."
       echo "  -c    Clear conversation history. Optionally, provide a number to clear a specific number of messages from the start. If no number is provided, it deletes all conversation history."
+      echo "  -n    Show the number of messages in the conversation."
       echo "  -h    Show this help message"
       echo
       echo "Note: All flags are mutually exclusive. You can only use one of -s, -l, -c, or -h at a time."
       echo "      The prompt is only allowed after the flags -s or -l. It should not be provided after -c or -h."
       exit 0
-      ;;
+    ;;
 
 
     \?)
       echo "Invalid option: -$OPTARG"
       exit 1
-      ;;
+    ;;
   esac
 done
 shift $((OPTIND -1))
